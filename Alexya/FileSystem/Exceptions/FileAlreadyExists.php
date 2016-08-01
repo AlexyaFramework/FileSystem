9d5b28db87b435778223a4d45651b0ded63470e2
manulaiko <manulaiko@gmail.com>
2016-08-01 21:02:14 +0200

    Created file exceptions

---
+++ Alexya/FileSystem/Exceptions/FileAlreadyExists.php
@@ -0,0 +1,30 @@
+<?php
+namespace Alexya\FileSystem\Exceptions;
+
+/**
+ * File already exists exception
+ *
+ * This exception is thrown when you try to create a file that already exists
+ *
+ * Example:
+ *
+ *     try {
+ *     	   $file = File::make("test.txt");
+ *     } catch(FileAlreadyExist $e) {
+ *         echo "File 'test.txt' already exists!";
+ *     }
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+class FileAlreadyExists extends Exception
+{
+    /**
+     * Constructor
+     *
+     * @param string $path Path to file
+     */
+    public function __construct(string $path)
+    {
+        parent::__construct("File '{$path}' already exists!");
+    }
+}
